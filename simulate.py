51026ebd5bfe9e6b4f35d787f2d92503bac10bb3
Fyodor Wolf <fwolf@bu.edu>
2014-10-06 17:21:36 -0400

    fixed optional argument inputs.

--- simulate.py
+++ simulate.py
@@ -8,12 +8,9 @@ class CPUScheduleSimluatorApp(CommandLineApp):
 
 	def setup(self):
 		super(CPUScheduleSimluatorApp,self).setup()
-		self.add_param("-f", "--file", help="Define the input file to run the simulator against. Default is `input.txt`.", 
-			default="input.txt", action="store_true")
-		self.add_param("-q", "--quantum", help="Round Robin allows for an optional quantum parameter. Default is "+str(Engine._time_quantum)+" cycles.", 
-			default=str(Engine._time_quantum), action="store_true")
-		self.add_param("algorithm", help="The algorithm to use when running the simulated CPU scheduler. Choices are: "+str(ALGORITHMS.keys()), 
-			default=False)
+		self.add_param("-f","--file", help="Define the input file to run the simulator against. Default is `input.txt`.", default="input.txt", required=False)
+		self.add_param("-q","--quantum", help="Round Robin allows for an optional quantum parameter. Default is "+str(Engine._time_quantum)+" cycles.", default=str(Engine._time_quantum), required=False)
+		self.add_param("algorithm", help="The algorithm to use when running the simulated CPU scheduler. Choices are: "+str(ALGORITHMS.keys()),  default=False)
 		
 	# This is called after run() completes setup()
 	def main(self):
